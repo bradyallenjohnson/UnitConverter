7108e63abc4b51c357ec05ef18eecbdcca0faf21
bdonvr <brandonvr5000@gmail.com>
2015-02-10 19:37:06 -0600

    Added required classes, switched to google currency API, added .DS_Store to .gitignore.

--- com/johnson/UnitConverter/units/MeasurementUnitCurrency.java
+++ com/johnson/UnitConverter/units/MeasurementUnitCurrency.java
@@ -1,46 +1,28 @@
 package com.johnson.UnitConverter.units;
 
 import java.io.IOException;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 public class MeasurementUnitCurrency implements CurrencyConverter{
-	    @SuppressWarnings("deprecation")
-		public float convert(String currencyFrom, String currencyTo) throws IOException {
-	        HttpClient httpclient = new DefaultHttpClient();
-	        HttpGet httpGet = new HttpGet("http://quote.yahoo.com/d/quotes.csv?s=" + currencyFrom + currencyTo + "=X&f=l1&e=.csv");
-	        ResponseHandler<String> responseHandler = new BasicResponseHandler();
-	        String responseBody = httpclient.execute(httpGet, responseHandler);
-	        httpclient.getConnectionManager().shutdown();
-	        return Float.parseFloat(responseBody);
+    public float convert(String currencyFrom, String currencyTo) throws IOException {
+    	Document doc = Jsoup.connect("https://www.google.com/finance/converter?a=1&from="+ currencyFrom +"&to=" + currencyTo).get();
+        Elements ps = doc.select("span");
+        return(Float.parseFloat((ps.text().substring(0, ps.text().length()-5))));
+    }
+    
+    //Get exchange rates
+    
+    public float getRate(String IR){
+    	MeasurementUnitCurrency getCurrentRate = new MeasurementUnitCurrency();
+    	try {
+    		return getCurrentRate.convert("USD", IR);
+    	}
+    	catch (Exception e) {
+	        e.printStackTrace();
 	    }
-	    
-	    //Get exchange rates
-	    
-	    public float getRate(String IR){
-	    	MeasurementUnitCurrency getCurrentRate = new MeasurementUnitCurrency();
-	    	try {
-	    		return getCurrentRate.convert("USD", IR);
-	    	}
-	    	catch (Exception e) {
-		        e.printStackTrace();
-		    }
-	    	return 0;
-	    }
-	    
-	    public static void main(String[] args) {
-	    	MeasurementUnitCurrency mcc = new MeasurementUnitCurrency();
-	        try {
-	            float current = mcc.convert("USD", "ILS");
-	            System.out.println(current);
-	        }
-	        catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-	}
+    	return 0;
+    }
 
+}
